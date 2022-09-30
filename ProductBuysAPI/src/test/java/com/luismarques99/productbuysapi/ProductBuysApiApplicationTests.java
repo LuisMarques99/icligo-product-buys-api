52258156f88a8c244a2030adf973eb325a1fbea2
LuisMarques99 <luisserafim99@hotmail.com>
2022-09-30 12:46:53 +0000

    ProductBuysAPI Baseline

---
+++ ProductBuysAPI/src/test/java/com/luismarques99/productbuysapi/ProductBuysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismarques99.productbuysapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBuysApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
