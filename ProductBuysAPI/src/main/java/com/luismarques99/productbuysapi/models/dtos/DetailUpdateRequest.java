7fcdbbd583827e871963304222ed512240205ba3
Luis Marques <luisserafim99@gmail.com>
2022-10-05 21:15:56 +0100

    Detail DTO's

---
+++ ProductBuysAPI/src/main/java/com/luismarques99/productbuysapi/models/dtos/DetailUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.luismarques99.productbuysapi.models.dtos;
+
+import com.luismarques99.productbuysapi.models.Detail;
+import lombok.AllArgsConstructor;
+
+/**
+ * Class that represents the structure of a {@link Detail detail} update request.
+ *
+ * @author Luis Marques
+ */
+@AllArgsConstructor
+public class DetailUpdateRequest extends DetailRequest {
+
+}
