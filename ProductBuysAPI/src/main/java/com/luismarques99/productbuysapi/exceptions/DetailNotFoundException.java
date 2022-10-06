bdf53734437df1960a5cb241e3a3f3fdf8726b3b
Luis Marques <luisserafim99@gmail.com>
2022-10-06 05:10:31 +0100

    Detail exceptions

---
+++ ProductBuysAPI/src/main/java/com/luismarques99/productbuysapi/exceptions/DetailNotFoundException.java
@@ -0,0 +1,22 @@
+package com.luismarques99.productbuysapi.exceptions;
+
+import com.luismarques99.productbuysapi.models.Detail;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Exception class that represents the structure of a not found {@link Detail detail} exception.
+ *
+ * @author Luis Marques
+ */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class DetailNotFoundException extends DetailException {
+
+    /**
+     * Creates a not found {@link Detail detail} exception.
+     */
+    public DetailNotFoundException(Long id) {
+        super("Could not find detail with id: " + id);
+    }
+
+}
